bc4c4f0efc49614c043d39e55a09d32a9c3e429e
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2021-12-21 12:30:59 +0100

    Add "solution" for day 23 prepared in Python
    I've solved this task using pen & paper, this script is used only to sum up all energy costs.
    I'll look into preparing "proper" solution later on.

--- day23/day23.py
+++ day23/day23.py
@@ -1,31 +1,39 @@
-from sys import argv
+"""
+I've solved this task using pen & paper rather than algorithms & programming.
+This script was used just to sum everything up.
+I'll look into a "proper" solution for this task later on.
+"""
 
-DEFAULT_INPUT_FILE_NAME = "input"
+AMPHIPODS_ENERGY_COST = {
+    "A": 1,
+    "B": 10,
+    "C": 100,
+    "D": 1000
+}
 
 
-def load_input():
-    with open(get_input_file_name()) as input_file:
-        return [line.strip() for line in input_file.readlines()]
-
-
-def get_input_file_name():
-    return argv[1] if len(argv) == 2 else DEFAULT_INPUT_FILE_NAME
-
-
-def part1(input):
-    print("Running part 1...")
-
-    print(f"Result:")
+def task(moves, part):
+    print(f"Running part {part}...")
+    energy_costs = [AMPHIPODS_ENERGY_COST[amphipod] * length for amphipod, length in moves.items()]
+    total_energy_cost = sum(energy_costs)
+    print(f"Total energy cost: {total_energy_cost}")
     print()
 
 
-def part2(input):
-    print("Running part 2...")
+# Solved using pen & paper
+part_1_moves = {
+    "A": 2 + 5 + 3 + 8,
+    "B": 2 + 3 + 4,
+    "C": 5 + 3 + 7,
+    "D": 5 + 5
+}
+task(part_1_moves, part=1)
 
-    print(f"Result:")
-    print()
-
-
-input = load_input()
-part1(input)
-part2(input)
+# Solved using pen & paper
+part_2_moves = {
+    "A": 11 + 11 + 6 + 8 + 8,
+    "B": 3 + 6 + 2 + 6 + 5 + 6 + 6 + 7,
+    "C": 5 + 8 + 7 + 5 + 6 + 7 + 5,
+    "D": 10 + 10 + 5 + 5 + 5 + 7,
+}
+task(part_2_moves, part=2)
